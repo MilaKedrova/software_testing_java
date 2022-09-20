64abba28517194ebf4d0a13ba2fd6e21f66d0212
MilaKedrova <luda_kedrova@mail.ru>
2022-09-20 11:25:32 +0300

    Исправлена ошибка в негативном тесте по созданию группы

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.ContactsData;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.testng.Assert.assertEquals;
@@ -18,12 +20,13 @@ public class ContactCreationTests extends TestBase {
         app.goTo().contactPage();
     }
 
-    @Test (enabled = false)
+    @Test
     public void testContactCreation() throws Exception {
         Contacts before = app.contact().all();
+        File photo = new File();
         ContactsData contact = new ContactsData().
                 withFirstName("Clarky").withLastName("Kent").withPhone("454545").
-                withEmail("superman@mail.ru").withAddress("smallville").withGroup("test10");
+                withEmail("superman@mail.ru").withAddress("smallville").withPhoto(photo);
         app.contact().create(contact, true);
         app.goTo().contactPage();
         Contacts after = app.contact().all();
@@ -33,11 +36,11 @@ public class ContactCreationTests extends TestBase {
         assertThat(after, equalTo(before.withAdded(contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
 
-    @Test
+    @Test (enabled = false)
     public void testBadContactCreation() throws Exception {
         Contacts before = app.contact().all();
         ContactsData contact = new ContactsData().
-                withFirstName("Clarky").withLastName("Kent").withPhone("454545").
+                withFirstName("Clarky'").withLastName("Kent").withPhone("454545").
                 withEmail("superman@mail.ru").withAddress("smallville").withGroup("test10");
         app.contact().create(contact, true);
         app.goTo().contactPage();
